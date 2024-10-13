e037bda7f15156bec665df714a17cceb8897bf8d
Daiki Okayama <lawliet.d.k.m@gmail.com>
2024-10-13 23:11:09 +0900

    Refactor code to separate handler logic into its own package

--- main.go
+++ main.go
@@ -3,12 +3,10 @@ package main
 import (
 	"log"
 
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/caarlos0/env/v11"
 	"github.com/line/line-bot-sdk-go/v8/linebot/messaging_api"
-	"github.com/okayama-daiki/kindle-daily-deals-notifier/libs/crawler"
-	"github.com/okayama-daiki/kindle-daily-deals-notifier/libs/notifier"
+	"github.com/okayama-daiki/kindle-daily-deals-notifier/libs/handler"
 )
 
 type config struct {
@@ -16,12 +14,12 @@ type config struct {
 	TargetId           string `env:"LINE_TARGET_ID"`
 }
 
-var (
-	cfg config
-	bot *messaging_api.MessagingApiAPI
-)
+func main() {
+	var (
+		cfg config
+		bot *messaging_api.MessagingApiAPI
+	)
 
-func init() {
 	if err := env.Parse(&cfg); err != nil {
 		log.Fatal(err)
 	}
@@ -33,32 +31,7 @@ func init() {
 	if err != nil {
 		log.Fatal(err)
 	}
-}
 
-func lambdaHandler(req events.LambdaFunctionURLRequest) (events.APIGatewayProxyResponse, error) {
-	products, err := crawler.Crawl()
-	if err != nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode: 500,
-		}, err
-	}
-
-	var messages []messaging_api.MessageInterface
-	for _, product := range products {
-		message := messaging_api.TextMessage{
-			Text: product.String(),
-		}
-		messages = append(messages, message)
-	}
-
-	notifier := notifier.New(bot)
-	notifier.Notify(cfg.TargetId, messages)
-
-	return events.APIGatewayProxyResponse{
-		StatusCode: 200,
-	}, nil
-}
-
-func main() {
-	lambda.Start(lambdaHandler)
+	handler := handler.New(bot, cfg.TargetId)
+	lambda.Start(handler.HandleRequest)
 }
