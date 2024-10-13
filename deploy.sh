6eae64a85110a057361110fafd179d323733dfa7
Daiki Okayama <lawliet.d.k.m@gmail.com>
2024-10-13 12:18:50 +0900

    Update function name

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function_name="linebotExample"
+function_name="notifyKindleDailyDeals"
 
 GOOS=linux GOARCH=arm64 go build -tags lambda.norpc -o bootstrap main.go
 zip myFunction.zip bootstrap
@@ -13,8 +13,9 @@ else
   aws lambda create-function \
     --function-name $function_name \
     --runtime provided.al2023 \
+    --architectures arm64 \
     --role arn:aws:iam::828951707561:role/awslambdaBasicExecutionRole \
-    --environment Variables="{LINE_CHANNEL_SECRET=YOUR_CHANNEL_SECRET,LINE_CHANNEL_ACCESS_TOKEN=YOUR_CHANNEL_ACCESS_TOKEN}" \
+    --environment Variables="{LINE_CHANNEL_ACCESS_TOKEN=YOUR_CHANNEL_ACCESS_TOKEN,LINE_TARGET_ID=YOUR_TARGET_ID}" \
     --handler bootstrap \
     --zip-file fileb://myFunction.zip
 fi
