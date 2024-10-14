f450535e43ee675c0d061b1d75943e4dcaa576ce
Daiki Okayama <lawliet.d.k.m@gmail.com>
2024-10-14 16:18:15 +0900

    Refactor code to separate handler logic into its own package

--- libs/handler/handler.go
+++ libs/handler/handler.go
@@ -5,46 +5,38 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/line/line-bot-sdk-go/v8/linebot/messaging_api"
+
 	"github.com/okayama-daiki/kindle-daily-deals-notifier/libs/crawler"
 	"github.com/okayama-daiki/kindle-daily-deals-notifier/libs/notifier"
 )
 
-type Handler struct {
-	bot      *messaging_api.MessagingApiAPI
-	targetId string
-}
-
-func New(bot *messaging_api.MessagingApiAPI, targetId string) *Handler {
-	return &Handler{
-		bot,
-		targetId,
-	}
-}
-
-func (h *Handler) HandleRequest(req events.LambdaFunctionURLRequest) (events.APIGatewayProxyResponse, error) {
-	products, err := crawler.Crawl()
-	if err != nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode: http.StatusInternalServerError,
-		}, err
-	}
-
-	var messages []messaging_api.MessageInterface
-	for _, product := range products {
-		message := messaging_api.TextMessage{
-			Text: product.String(),
+func Handler(bot *messaging_api.MessagingApiAPI, targetId string) func(events.LambdaFunctionURLRequest) (events.APIGatewayProxyResponse, error) {
+	return func(req events.LambdaFunctionURLRequest) (events.APIGatewayProxyResponse, error) {
+		products, err := crawler.Crawl()
+		if err != nil {
+			return events.APIGatewayProxyResponse{
+				StatusCode: http.StatusInternalServerError,
+			}, err
 		}
-		messages = append(messages, message)
-	}
 
-	notifier := notifier.New(h.bot)
-	if err := notifier.Notify(h.targetId, messages); err != nil {
+		var messages []messaging_api.MessageInterface
+		for _, product := range products {
+			message := messaging_api.TextMessage{
+				Text: product.String(),
+			}
+			messages = append(messages, message)
+		}
+
+		notifier := notifier.New(bot)
+		if err := notifier.Notify(targetId, messages); err != nil {
+			return events.APIGatewayProxyResponse{
+				StatusCode: http.StatusInternalServerError,
+			}, err
+		}
+
 		return events.APIGatewayProxyResponse{
-			StatusCode: http.StatusInternalServerError,
-		}, err
+			StatusCode: http.StatusOK,
+		}, nil
 	}
 
-	return events.APIGatewayProxyResponse{
-		StatusCode: http.StatusOK,
-	}, nil
 }
--- main.go
+++ main.go
@@ -3,16 +3,14 @@ package main
 import (
 	"os"
 
+	"github.com/caarlos0/env/v11"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 
-	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/caarlos0/env/v11"
-	"github.com/line/line-bot-sdk-go/v8/linebot/messaging_api"
-	"github.com/okayama-daiki/kindle-daily-deals-notifier/libs/handler"
+	"github.com/okayama-daiki/kindle-daily-deals-notifier/libs/lambda"
 )
 
-type config struct {
+type Config struct {
 	ChannelAccessToken string `env:"LINE_CHANNEL_ACCESS_TOKEN"`
 	TargetId           string `env:"LINE_TARGET_ID"`
 }
@@ -20,19 +18,18 @@ type config struct {
 func main() {
 	zerolog.SetGlobalLevel(zerolog.InfoLevel)
 
-	var cfg config
+	var cfg Config
 
 	if err := env.Parse(&cfg); err != nil {
 		log.Error().Err(err)
 		os.Exit(1)
 	}
 
-	bot, err := messaging_api.NewMessagingApiAPI(cfg.ChannelAccessToken)
+	lambda, err := lambda.New(cfg.ChannelAccessToken, cfg.TargetId)
 	if err != nil {
 		log.Error().Err(err)
 		os.Exit(1)
 	}
 
-	handler := handler.New(bot, cfg.TargetId)
-	lambda.Start(handler.HandleRequest)
+	lambda.Start()
 }
